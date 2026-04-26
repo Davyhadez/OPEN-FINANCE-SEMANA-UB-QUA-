603e3cf2179953450758775c4c0d50d1fec62ca0
Davyhadez <davidlgk2267@gmail.com>
2026-04-26 02:04:17 -0300

    fix: redirecionar para login após o cadastro

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -43,9 +43,8 @@ class RegisteredUserController extends Controller
         ]);
 
         event(new Registered($user));
+    
 
-        Auth::login($user);
-
-        return redirect(route('dashboard', absolute: false));
+        return redirect()->route('login')->with('status', 'Cadastro realizado! Por favor, faça o login.');
     }
 }
